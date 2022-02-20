1f5b78fef8302f16c73f804eddef650c44e0f9ee
test1234455 <ingnoscemihi@gmail.com>
2022-02-20 13:35:57 +0800

    Update rechaindb.sh

--- rechaindb.sh
+++ rechaindb.sh
@@ -14,6 +14,8 @@ dopullchaindb(){
   done
 }
 systemctl stop nkn-commercial.service
+ps -ef |grep NKN | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
 dopullchaindb
 chown -R nknx:nknx /home/nknx
 chmod -R 755 /home/nknx
