514d4fa87c9b2727f8410a9f1d73601ec70c1984
test1234455 <ingnoscemihi@gmail.com>
2022-02-20 13:17:29 +0800

    Create rechaindb.sh

---
+++ rechaindb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /home/nknx/nkn-commercial/services/nkn-node/
+dopullchaindb(){
+  wget -c --no-check-certificate https://pan.ssccc.workers.dev/ChainDB.tar.gz -O - | tar -xz
+  filesize=`du "ChainDB" | awk '{ print $1 }'`
+  echo ${filesize}
+  while [[ $filesize -lt 19000000 ]];
+  do
+    echo "redownload chianDB"
+    rm -rf ChainDB
+    wget -c --no-check-certificate https://pan.ssccc.workers.dev/ChainDB.tar.gz -O - | tar -xz
+  done
+}
+dopullchaindb
+chown -R nknx:nknx /home/nknx
+chmod -R 755 /home/nknx
+chown -R nknx:nknx ChainDB/
+
+systemctl restart nkn-commercial.service
+
+echo "DONE wait for node online"
