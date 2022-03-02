51ae6c82500cc22cba5093702a71548361940cf5
test1234455 <ingnoscemihi@gmail.com>
2022-03-02 10:33:20 +0800

    Update rechaindb.sh

--- rechaindb.sh
+++ rechaindb.sh
@@ -16,6 +16,8 @@ dopullchaindb(){
 systemctl stop nkn-commercial.service
 ps -ef |grep NKN | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
 dopullchaindb
 chown -R nknx:nknx /home/nknx
 chmod -R 755 /home/nknx
