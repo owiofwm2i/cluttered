eb229f66cb00f9a25398f79ff653d0c690dbe8b0
test1234455 <ingnoscemihi@gmail.com>
2022-03-02 10:48:01 +0800

    Update rechaindb.sh

--- rechaindb.sh
+++ rechaindb.sh
@@ -14,7 +14,7 @@ dopullchaindb(){
   done
 }
 systemctl stop nkn-commercial.service
-ps -ef |grep /dev/fd/63 | grep -v grep | awk '{print $2}' | xargs kill -9
+
 ps -ef |grep NKN | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef |grep wget | grep -v grep | awk '{print $2}' | xargs kill -9
